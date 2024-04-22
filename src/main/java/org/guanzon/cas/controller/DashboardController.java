714b04fbc9f11920d1d5a87ae40cdb3cc6b67547
Jeitee <93184955+jeitee@users.noreply.github.com>
2024-04-22 13:23:24 +0800

    cas ui
    4/22

--- src/main/java/org/guanzon/cas/controller/DashboardController.java
+++ src/main/java/org/guanzon/cas/controller/DashboardController.java
@@ -23,23 +23,40 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collections;
 import java.util.Date;
+import java.util.List;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.animation.FadeTransition;
+import javafx.beans.binding.Bindings;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
+import javafx.geometry.Bounds;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Tab;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
-import javafx.stage.StageStyle;
+
 
 public class DashboardController implements Initializable {
     private GRider oApp;
     private Stage stage;
+    
+    private int targetTabIndex = -1;
+    private double tabsize;
+    
+    List<String> tabName = new ArrayList<>();
 
     @FXML
     private Button btnClose, btnMinimize;
@@ -112,8 +129,250 @@ public class DashboardController implements Initializable {
         }
     }
 
-    private void setTabPane() {
-        // Implement your tab pane setup here
+    public TabPane loadAnimate(String fsFormName) {
+        //set fxml controller class
+        if (tabpane.getTabs().isEmpty()) {
+            tabpane = new TabPane();
+        }
+
+        setTabPane();
+
+        ScreenInterface fxObj = getController(fsFormName);
+        fxObj.setGRider(oApp);
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(fxObj.getClass().getResource(fsFormName));
+        fxmlLoader.setController(fxObj);
+
+        //Add new tab;
+        Tab newTab = new Tab(SetTabTitle(fsFormName));
+        newTab.setStyle("-fx-font-weight: bold; -fx-pref-width: 180; -fx-font-size: 10.5px; -fx-border-color: #F88222 #F88222 transparent #F88222;");
+        //tabIds.add(fsFormName);
+        newTab.setContent(new javafx.scene.control.Label("Content of Tab " + fsFormName));
+        newTab.setContextMenu(createContextMenu(tabpane, newTab, oApp));
+        // Attach a context menu to each tab
+        tabName.add(SetTabTitle(fsFormName));
+
+        // Save the list of tab IDs to the JSON file
+//        TabsStateManager.saveCurrentTab(tabName);
+        try {
+            Node content = fxmlLoader.load();
+            newTab.setContent(content);
+            tabpane.getTabs().add(newTab);
+            tabpane.getSelectionModel().select(newTab);
+            
+            //newTab.setOnClosed(event -> {
+            newTab.setOnCloseRequest(event -> {
+                if (showMessage()){
+                    Tabclose();
+                    tabName.remove(newTab.getText());
+                } else {
+                    event.consume();
+                }
+                
+
+
+            });
+
+            newTab.setOnSelectionChanged(event -> {
+                ObservableList<Tab> tabs = tabpane.getTabs();
+                for (Tab tab : tabs) {
+                    if (tab.getText().equals(newTab.getText())) {
+                        tabName.remove(newTab.getText());
+                        tabName.add(newTab.getText());
+                        // Save the list of tab IDs to the JSON file
+//                        TabsStateManager.saveCurrentTab(tabName);
+                        break;
+                    }
+                }
+
+            });
+            return (TabPane) tabpane;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+    
+    //Load Main Screen if no tab remain
+    public void Tabclose() {
+        int tabsize = tabpane.getTabs().size();
+        if (tabsize == 1) {
+            setScene(loadAnimateAnchor("FXMLMainScreen.fxml"));
+        }
+    }
+    public String SetTabTitle(String menuaction) {
+        switch (menuaction) {
+            /*DIRECTORY*/
+            case "/org/guanzon/cas/views/ClientMasterParameter.fxml":
+                return "Client Parameter";
+            case "/org/guanzon/cas/views/ClientMasterTransactionCompany.fxml":
+                return "Client Transactions Company";
+            case "/org/guanzon/cas/views/ClientMasterTransactionIndividual.fxml":
+                return "Client Transactions Individual";
+            default:
+                return null;
+        }
+    }
+    public ContextMenu createContextMenu(TabPane tabPane, Tab tab, GRider oApp) {
+        ContextMenu contextMenu = new ContextMenu();
+
+        MenuItem closeTabItem = new MenuItem("Close Tab");
+        MenuItem closeOtherTabsItem = new MenuItem("Close Other Tabs");
+        MenuItem closeAllTabsItem = new MenuItem("Close All Tabs");
+
+        closeTabItem.setOnAction(event -> closeSelectTabs(tabPane, tab));
+        closeOtherTabsItem.setOnAction(event -> closeOtherTabs(tabPane, tab));
+        closeAllTabsItem.setOnAction(event -> closeAllTabs(tabPane, oApp));
+
+        contextMenu.getItems().add(closeTabItem);
+        contextMenu.getItems().add(closeOtherTabsItem);
+        contextMenu.getItems().add(closeAllTabsItem);
+
+        tab.setContextMenu(contextMenu);
+
+        closeOtherTabsItem.visibleProperty().bind(Bindings.size(tabPane.getTabs()).greaterThan(1));
+
+        return contextMenu;
+    }
+    private void closeSelectTabs(TabPane tabPane, Tab tab) {
+        if (showMessage()){
+            Tabclose(tabPane);
+            tabName.remove(tab.getText());
+            tabPane.getTabs().remove(tab);
+        }
+//        if (ShowMessageFX.YesNo(null, "Close Tab", "Are you sure, do you want to close tab?") == true) {
+            
+//            TabsStateManager.saveCurrentTab(tabName);
+//            TabsStateManager.closeTab(tab.getText());
+            
+//        }
+
+    }
+    //Load Main Screen if no tab remain
+
+    public void Tabclose(TabPane tabpane) {
+        int tabsize = tabpane.getTabs().size();
+        if (tabsize == 1) {
+            setScene(loadAnimateAnchor("Dashboard.fxml"));
+        }
+    }
+    private void closeOtherTabs(TabPane tabPane, Tab currentTab) {
+//        if (ShowMessageFX.YesNo(null, "Close Other Tab", "Are you sure, do you want to close other tab?") == true) {
+          if (showMessage()){
+            tabPane.getTabs().removeIf(tab -> tab != currentTab);
+            List<String> currentTabNameList = Collections.singletonList(currentTab.getText());
+            tabName.retainAll(currentTabNameList);
+//            TabsStateManager.saveCurrentTab(tabName);
+            for (Tab tab : tabPane.getTabs()) {
+                String formName = tab.getText();
+//                TabsStateManager.closeTab(formName);
+            }
+          }
+//        }
+    }
+
+    private void closeAllTabs(TabPane tabPane, GRider oApp) {
+//        if (ShowMessageFX.YesNo(null, "Close All Tabs", "Are you sure, do you want to close all tabs?") == true) {
+          if (showMessage()){
+            tabName.clear();
+//            TabsStateManager.saveCurrentTab(tabName);
+            // Close all tabs using your TabsStateManager
+            for (Tab tab : tabPane.getTabs()) {
+                String formName = tab.getText();
+//                TabsStateManager.closeTab(formName);
+            }
+            tabPane.getTabs().clear();
+//            unloadForm unload = new unloadForm();
+            StackPane myBox = (StackPane) tabpane.getParent();
+            myBox.getChildren().clear();
+//            myBox.getChildren().add(unload.getScene("FXMLMainScreen.fxml", oApp));
+          }
+        
+    }
+    public void setTabPane() {
+        // set up the drag and drop listeners on the tab pane
+        tabpane.setOnDragDetected(event -> {
+            Dragboard db = tabpane.startDragAndDrop(TransferMode.MOVE);
+            ClipboardContent content = new ClipboardContent();
+            content.putString(tabpane.getSelectionModel().getSelectedItem().getText());
+            db.setContent(content);
+            event.consume();
+        });
+
+        tabpane.setOnDragOver(event -> {
+            Dragboard db = event.getDragboard();
+            if (db.hasString()) {
+                event.acceptTransferModes(TransferMode.MOVE);
+                event.consume();
+            }
+        });
+
+        tabpane.setOnDragDropped(event -> {
+            Dragboard db = event.getDragboard();
+            boolean success = false;
+            if (db.hasString()) {
+                String tabText = db.getString();
+                int draggedTabIndex = findTabIndex(tabText);
+                //double mouseP , mousePCom;
+                double mouseX = event.getX();
+                double mouseY = event.getY();
+                Bounds headerBounds = tabpane.lookup(".tab-header-area").getBoundsInParent();
+                Point2D mouseInScene = tabpane.localToScene(mouseX, mouseY);
+                Point2D mouseInHeader = tabpane.sceneToLocal(mouseInScene);
+                double tabHeaderHeight = tabpane.lookup(".tab-header-area").getBoundsInParent().getHeight();
+                System.out.println("mouseY " + mouseY);
+                System.out.println("tabHeaderHeight " + tabHeaderHeight);
+
+//                    mouse is over the tab header area
+//                    mouseP = ((mouseInHeader.getX() / headerBounds.getWidth()));
+//                    tabsize = tabpane.getTabs().size();
+//                    mousePCom = mouseP * tabsize;
+//                    targetTabIndex = (int) Math.round(mousePCom) ;
+//
+//                    double tabWidth = headerBounds.getWidth() / tabpane.getTabs().size();
+//                    targetTabIndex = (int) ((mouseX - headerBounds.getMinX()) / tabWidth);
+                targetTabIndex = (int) (mouseX / 180);
+                System.out.println("targetTabIndex " + targetTabIndex);
+                if (mouseY < tabHeaderHeight) {
+                    //if (headerBounds.contains(mouseInHeader)) {
+                    System.out.println("mouseInHeader.getX() " + mouseInHeader.getX());
+                    System.out.println("headerBounds.getWidth() " + headerBounds.getWidth());
+                    System.out.println("tabsize " + tabpane.getTabs().size());
+                    System.out.println("tabText " + tabText);
+                    System.out.println("draggedTabIndex " + draggedTabIndex);
+
+                    if (draggedTabIndex != targetTabIndex) {
+                        Tab draggedTab = tabpane.getTabs().remove(draggedTabIndex);
+                        if (targetTabIndex > tabpane.getTabs().size()) {
+                            targetTabIndex = tabpane.getTabs().size();
+                        }
+                        tabpane.getTabs().add(targetTabIndex, draggedTab);
+                        tabpane.getSelectionModel().select(draggedTab);
+                        success = true;
+
+                    }
+                    //}
+                }
+            }
+            event.setDropCompleted(success);
+            event.consume();
+        });
+
+        tabpane.setOnDragDone(event -> {
+            event.consume();
+        });
+
+    }
+    private int findTabIndex(String tabText) {
+        ObservableList<Tab> tabs = tabpane.getTabs();
+        for (int i = 0; i < tabs.size(); i++) {
+            if (tabs.get(i).getText().equals(tabText)) {
+                return i;
+            }
+        }
+        return -1;
     }
 
     private void getTime() {
@@ -133,7 +392,6 @@ public class DashboardController implements Initializable {
             String formattedDate = dateFormat.format(date);
 
             AppDateTime.setText(formattedDate + " | " + formattedTime);
-
         }),
                 new KeyFrame(Duration.seconds(1))
         );
@@ -158,11 +416,7 @@ public class DashboardController implements Initializable {
                     Platform.exit();
                     break;
                 case "btnMinimize":
-                    if (stage != null) {
-                        stage.setIconified(true); // Try to minimize the stage
-                    } else {
-                        System.err.println("Stage is not properly initialized.");
-                    }
+                    stage.setIconified(true); // Try to minimize the stage
 //                    stage.setIconified(true);
 //                    stage.initStyle(StageStyle.DECORATED);
                     break;
@@ -170,15 +424,16 @@ public class DashboardController implements Initializable {
             }
         }
     }
-    
-    private AnchorPane loadAnimate(String fsFormName){
+     /*LOAD ANIMATE FOR ANCHORPANE MAIN HOME*/
+    public AnchorPane loadAnimateAnchor(String fsFormName) {
+
         ScreenInterface fxObj = getController(fsFormName);
         fxObj.setGRider(oApp);
-       
+
         FXMLLoader fxmlLoader = new FXMLLoader();
-        fxmlLoader.setLocation(fxObj.getClass().getResource( fsFormName));
-        fxmlLoader.setController(fxObj);    
-        
+        fxmlLoader.setLocation(fxObj.getClass().getResource(fsFormName));
+        fxmlLoader.setController(fxObj);
+
         AnchorPane root;
         try {
             root = (AnchorPane) fxmlLoader.load();
@@ -189,29 +444,132 @@ public class DashboardController implements Initializable {
             ft.setCycleCount(1);
             ft.setAutoReverse(false);
             ft.play();
-
+            
             return root;
         } catch (IOException ex) {
             System.err.println(ex.getMessage());
-            
         }
+
         return null;
     }
+    
+//    private AnchorPane loadAnimate(String fsFormName){
+//        ScreenInterface fxObj = getController(fsFormName);
+//        fxObj.setGRider(oApp);
+//       
+//        FXMLLoader fxmlLoader = new FXMLLoader();
+//        fxmlLoader.setLocation(fxObj.getClass().getResource( fsFormName));
+//        fxmlLoader.setController(fxObj);    
+//        
+//        AnchorPane root;
+//        try {
+//            root = (AnchorPane) fxmlLoader.load();
+//            FadeTransition ft = new FadeTransition(Duration.millis(1500));
+//            ft.setNode(root);
+//            ft.setFromValue(1);
+//            ft.setToValue(1);
+//            ft.setCycleCount(1);
+//            ft.setAutoReverse(false);
+//            ft.play();
+//
+//            return root;
+//        } catch (IOException ex) {
+//            System.err.println(ex.getMessage());
+//            
+//        }
+//        return null;
+//    }
     private ScreenInterface getController(String fsValue){
         switch (fsValue){
-            case "/com/rmj/guanzongroup/cas/maven/views/ClientMasterParameter.fxml":
+            case "/org/guanzon/cas/views/ClientMasterParameter.fxml":
                 return new ClientMasterParameterController();
+                
+            case "/org/guanzon/cas/views/ClientMasterTransactionCompany.fxml":
+                return new ClientMasterTransactionCompanyController();
+                
+            case "/org/guanzon/cas/views/ClientMasterTransactionIndividual.fxml":
+                return new ClientMasterTransactionIndividualController();
+                
             default:
-            return null;
+                return null;
         }
     }
     private void setScene(AnchorPane foPane){
         workingSpace.getChildren().clear();
         workingSpace.getChildren().add(foPane);
     }
+    public int checktabs(String tabtitle) {
+        for (Tab tab : tabpane.getTabs()) {
+            if (tab.getText().equals(tabtitle)) {
+                tabpane.getSelectionModel().select(tab);
+                return 0;
+            }
+        }
+        return 1;
+    }
+        /*SET SCENE FOR WORKPLACE - STACKPANE - TABPANE*/
+    public void setScene2(TabPane foPane) {
+        workingSpace.getChildren().clear();
+        workingSpace.getChildren().add(foPane);
+    }
+    
+    /*MENU ACTIONS OPENING FXML's*/
     @FXML
     private void mnuClientParameterClick(ActionEvent event) {
-        setScene(loadAnimate("/com/rmj/guanzongroup/cas/maven/views/ClientMasterParameter.fxml"));
-    } 
+    String sformname = "/org/guanzon/cas/views/ClientMasterParameter.fxml";
+    //check if oApp is not null before calling loadAnimate
+    if (oApp != null && checktabs(SetTabTitle(sformname)) == 1) {
+        setScene2(loadAnimate(sformname));
+    }
+    }
+    
+    @FXML
+    private void mnuClientTransactionCompanyClick(ActionEvent event) {
+        String sformname = "/org/guanzon/cas/views/ClientMasterTransactionCompany.fxml";
+        //check tab
+        if (checktabs(SetTabTitle(sformname)) == 1) {
+            setScene2(loadAnimate(sformname));
+        }
+    }
+    @FXML
+    private void mnuClientTransactionIndividualClick(ActionEvent event) {
+        String sformname = "/org/guanzon/cas/views/ClientMasterTransactionIndividual.fxml";
+        //check tab
+        if (checktabs(SetTabTitle(sformname)) == 1) {
+            setScene2(loadAnimate(sformname));
+        }
+    }
+//    @FXML
+//    private void mnuClientParameterClick(ActionEvent event) {
+//        setScene(loadAnimate("/com/rmj/guanzongroup/cas/maven/views/ClientMasterParameter.fxml"));
+//    } 
+//    @FXML
+//    private void mnuClientTransactionCompanyClick(ActionEvent event) {
+//        setScene(loadAnimate("/com/rmj/guanzongroup/cas/maven/views/ClientMasterTransactionCompany.fxml"));
+//    } 
+//    @FXML
+//    private void mnuClientTransactionIndividualClick(ActionEvent event) {
+//        setScene(loadAnimate("/com/rmj/guanzongroup/cas/maven/views/ClientMasterTransactionCompany.fxml"));
+//    } 
+    
+    private boolean showMessage() {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Are you sure you want to proceed?");
+        alert.setContentText("Choose your option.");
+
+        // Add Yes and No buttons to the alert dialog
+        ButtonType buttonTypeYes = new ButtonType("Yes");
+        ButtonType buttonTypeNo = new ButtonType("No");
+        alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeNo);
+
+        // Show the alert and wait for a response
+        javafx.scene.control.ButtonType result = alert.showAndWait().orElse(ButtonType.CANCEL);
+
+        // Handle the user's response
+        return result == buttonTypeYes;
+    }
+
+    
 }
 
